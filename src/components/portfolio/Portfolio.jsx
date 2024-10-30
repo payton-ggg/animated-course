c08b7f1c93e5b9e158a6b14fb28a0111806b8c7c
Platon <platon20081019@gmail.com>
2024-10-30 18:51:50 +0200

    вапыыф

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -6,42 +6,42 @@ const items = [
   {
     id: 1,
     title: "Що таке Всесвіт?",
-    img: "http://res.cloudinary.com/platon1gg/image/upload/v1730238315/yf7dv6cvdyr9hu3h9zbh.jpg",
+    img: "http://res.cloudinary.com/platon1gg/image/upload/v1730306782/imfu3uuqoqszz8fbtmhw.jpg",
     desc: "Учні дізнаються про Всесвіт, його структуру та масштаби, дослідять галактики, зоряні системи та наше місце у Чумацькому Шляху, формуючи уявлення про величезність космосу.",
   },
   {
     id: 2,
     title: "Зірки — наше космічне минуле та майбутнє",
-    img: "http://res.cloudinary.com/platon1gg/image/upload/v1730238315/xzil8clpf6sfuu2idlef.jpg",
+    img: "http://res.cloudinary.com/platon1gg/image/upload/v1730306782/jy0sr3kiifotv3zbyfg2.jpg",
     desc: "На уроці розглянемо, як народжуються, живуть та вмирають зірки, дослідимо червоних гігантів, наднові і білі карлики, а також їхню роль у космосі.",
   },
   {
     id: 3,
     title: "Сонячна система та її таємниці",
-    img: "http://res.cloudinary.com/platon1gg/image/upload/v1730238315/jbto4uxhjqg3dxees8zp.jpg",
+    img: "http://res.cloudinary.com/platon1gg/image/upload/v1730306782/snnpz4v38zdr83xdjl6w.jpg",
     desc: "Учні познайомляться з планетами Сонячної системи, їх фізичними характеристиками, кліматом, а також дізнаються, чим особливі кільця деяких планет.",
   },
   {
     id: 4,
     title: "Екзопланети та пошуки життя за межами Землі",
-    img: "http://res.cloudinary.com/platon1gg/image/upload/v1730238315/y1qk2nchihhojlpfzrxi.jpg",
+    img: "http://res.cloudinary.com/platon1gg/image/upload/v1730306782/kwjfsww4do0vxebxuh1t.jpg",
     desc: "Учні дізнаються про екзопланети, способи їхнього виявлення, а також зрозуміють, чому пошук потенційно населених світів є важливим для науки.",
   },
   {
     id: 5,
-    title: "Екзопланети та пошуки життя за межами Землі",
+    title: "Чорні діри та загадки Всесвіту",
     img: "http://res.cloudinary.com/platon1gg/image/upload/v1730238315/xzil8clpf6sfuu2idlef.jpg",
     desc: "Розглянемо чорні діри, їхню структуру, як вони утворюються і викривлюють простір і час. Учні дізнаються, чому ці об'єкти унікальні та загадкові.",
   },
   {
     id: 6,
-    title: "Екзопланети та пошуки життя за межами Землі",
+    title: "Космічні подорожі та майбутнє людства",
     img: "http://res.cloudinary.com/platon1gg/image/upload/v1730238315/xzil8clpf6sfuu2idlef.jpg",
     desc: "Обговоримо сучасні та майбутні космічні місії, можливості освоєння Марсу, технології космічних подорожей і труднощі колонізації інших планет.",
   },
   {
     id: 7,
-    title: "Екзопланети та пошуки життя за межами Землі",
+    title: "Підсумкове заняття та проєктна робота",
     img: "http://res.cloudinary.com/platon1gg/image/upload/v1730238315/xzil8clpf6sfuu2idlef.jpg",
     desc: "Учні закріплять матеріали, презентуючи свої міні-проєкти на улюблені теми курсу, що розвине навички командної роботи і поглибить знання.",
   },
@@ -88,7 +88,7 @@ const Portfolio = () => {
   });
 
   return (
-    <div className="portfolio" ref={ref}>
+    <div className="program" ref={ref}>
       <div className="progress">
         <h1>Program</h1>
         <motion.div style={{ scaleX }} className="progressBar"></motion.div>
